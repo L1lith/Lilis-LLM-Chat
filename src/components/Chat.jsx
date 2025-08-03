8464cb38129a031b64a38e4070b82bfce9a97ded
BuildTools <lily@l1lith.com>
2025-08-02 21:08:15 -0600

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,17 +1,21 @@
 import MessageHistory from "./MessageHistory"
 import { createSignal, onCleanup, onMount } from "solid-js"
 import Send from '../icons/send.svg?raw'
+import Settings from '../icons/settings.svg?raw'
+import Inbox from '../icons/inbox.svg?raw'
 import "../styles/chat.scss"
 
 export default function Chat() {
     let input = null
     let submitButton = null
+    const [chatUnfocused, setChatUnfocused] = createSignal(false)
     const [messages, setMessages] = createSignal([])
     const [awaitingResponse, setAwaitingResponse] = createSignal(false)
 
 
     const autoFocus = () => {
-            input.focus()
+        if (chatUnfocused()) return
+        input.focus()
     };
     onMount(()=>{
         if (typeof global?.window !== 'undefined') {
@@ -30,6 +34,7 @@ export default function Chat() {
         const prompt = input.value.trim()
         if (!prompt.trim()) return
         input.value=''
+        autoAdjustInputHeight()
         setAwaitingResponse(true)
         let AIResponse
         try {
@@ -65,11 +70,16 @@ export default function Chat() {
             isShiftDown = false
         }
     }
+    const autoAdjustInputHeight = ()=>{
+        input.style.height = "";input.style.height = `min(calc(${input.scrollHeight}px - 2em), 30vh)`
+    }
 
     return <div class="chat">
+        <div class="inbox" innerHTML={Inbox}></div>
+        <div class="settings" innerHTML={Settings}></div>
         <MessageHistory messages={messages}/>
         <form class="prompter" onSubmit={handleSubmit}>
-            <textarea onInput={function(){this.style.height = "";this.style.height = `min(calc(${this.scrollHeight}px - 2em), 30vh)`}} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} class="prompt" disabled={awaitingResponse()} ref={input}/>
+            <textarea onInput={autoAdjustInputHeight} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} class="prompt" disabled={awaitingResponse()} ref={input}/>
             <button ref={submitButton} type="submit"><div innerHTML={Send}></div></button>
         </form>
     </div>
