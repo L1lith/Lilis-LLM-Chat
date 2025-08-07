abf21e5967da675570ae9d3e66a25ccb89da5ca1
BuildTools <lily@l1lith.com>
2025-08-06 21:30:37 -0600

    update builder scripts

--- NWJS/runBuilder.js
+++ NWJS/runBuilder.js
@@ -1,4 +1,16 @@
 import pkg from "../package.json" with {type: 'json'};
 import nwbuild from "nw-builder";
+import {join} from 'path'
+import { writeFile } from "fs/promises";
 
-nwbuild({...pkg.nwbuild, cacheDir: "./node_modules/nw"});
+await nwbuild({...pkg.nwbuild, cacheDir: "./node_modules/nw", platform: 'osx'});
+
+const bundleDir = join(import.meta.dirname, '..', 'bundle')
+const tempPackagePath = join(bundleDir, 'package.json')
+const tempPackage = {...pkg}
+delete tempPackage.devDependencies
+
+await writeFile(tempPackagePath, JSON.stringify(tempPackage))
+console.log("wrote new package at", tempPackagePath)
+
+process.exit(0)
